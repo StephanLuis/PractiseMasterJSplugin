ca032438b313b7ea022d073c18063f08bf38bd8f
StephanLuis <stephan@learnsense.org>
2023-10-12 10:35:56 +0100

    Update PMbundle.js

--- nonobfs/PMbundle.js
+++ nonobfs/PMbundle.js
@@ -1,4 +1,8 @@
 /*
+*12 Oct 2023
+*This bundle is for limited pre-commercial distribution
+* If you require a license constact stephan@learnsense.org
+*
  * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
  * It uses "eval()" calls to create a separate source file in the browser devtools.
